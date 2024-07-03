5aaf8edffaffb45d6e8c6dbb5c499b315c7075c6
Luiz Otávio <luizotavioautomacao@gmail.com>
2024-07-03 20:28:58 -0300

    refactor: add admin userSchema

--- src/modules/user/user.schema.ts
+++ src/modules/user/user.schema.ts
@@ -13,6 +13,9 @@ export class User {
 
   @Prop({ required: true })
   password: string;
+
+  @Prop({ default: false })
+  admin: boolean;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);

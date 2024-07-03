4e4a54f8c6c56960d5601a965f820cec68183864
Luiz Otávio <luizotavioautomacao@gmail.com>
2024-07-03 20:33:47 -0300

    refactor: delete user controller and move the logical to auth controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,52 @@
-import { Controller, Post, UseGuards, Request, Body } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Get, Post, Param, Request, Body, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
+import { EmailInUseError } from 'src/presentation/errors/email-in-use-error';
+import { EmailValidation } from 'src/presentation/validators/email-validation';
+import { EmailValidatorAdapter } from 'src/infra/validators/email-validator-adapter';
+import { RequiredFieldValidation } from 'src/presentation/validators/required-field-validation';
+import { JwtAuthGuard } from './jwt.auth.guard';
+import { User } from '../user/user.interface';
+import { UnauthorizedError } from 'src/presentation/validators/unauthorized-error';
+
+const makeRequiredValidation = (input, field) => {
+    const requiredFieldValidation = new RequiredFieldValidation(field)
+    const error = requiredFieldValidation.validate(input)
+    if (error) return error
+}
+
+const makeEmailValidation = (input) => {
+    const emailValidation = new EmailValidation('email', new EmailValidatorAdapter())
+    return emailValidation.validate(input)
+}
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) { }
+    constructor(
+        private readonly authService: AuthService,
+        private readonly userService: UserService) { }
+
+    async registerValidate(input: any): Promise<Error> {
+        for (const field of ['username', 'email', 'password']) {
+            const error = makeRequiredValidation(input, field)
+            if (error) return error
+        }
+        return makeEmailValidation(input)
+    }
+
+    async loginValidate(input: any): Promise<Error> {
+        for (const field of ['email', 'password']) {
+            const error = makeRequiredValidation(input, field)
+            if (error) return error
+        }
+        return makeEmailValidation(input)
+    }
 
     @Post('login')
     async login(@Body() body: { email: string; password: string }) {
+        const { email, password } = body
+        const error = this.loginValidate({ email, password })
+        if (error) return error
         const user = await this.authService.validateUser(body);
         if (!user) return { error: 'Credenciais inválidas' };
         return this.authService.login(user);
@@ -15,12 +54,29 @@ export class AuthController {
 
     @Post('register')
     async register(@Body() body: { username: string; email: string; password: string }) {
-        // Implemente a lógica de registro aqui (validação, criação de usuário, etc.)
+        const { username, email, password } = body
+        const error = this.registerValidate({ username, email, password })
+        if (error) return error
+        const user = await this.userService.getOneUserByQuery({ email })
+        if (user) return new EmailInUseError()
+        return this.userService.createUser(body);
     }
 
-    @UseGuards(AuthGuard('jwt'))
-    @Post('profile')
-    getProfile(@Request() req) {
-        return req.user;
+    @Get('users')
+    @UseGuards(JwtAuthGuard)
+    async getAllUsers(@Request() req): Promise<User[] | Error> {
+        if (!req.user.admin) {
+            return new UnauthorizedError()
+        }
+        return this.userService.getAllUsers();
+    }
+
+    @Get('user/:id')
+    @UseGuards(JwtAuthGuard)
+    async getUserById(@Param('id') id: string, @Request() req): Promise<User | Error> {
+        if (req.user._id !== id) {
+            return new UnauthorizedError()
+        }
+        return this.userService.getUserById(id);
     }
 }

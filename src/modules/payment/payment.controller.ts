075730e739886a88eff7a0667bb30e7a7735cc05
Luiz Otávio <luizotavioautomacao@gmail.com>
2024-07-04 00:46:50 -0300

    feat: add create payment in controller

--- src/modules/payment/payment.controller.ts
+++ src/modules/payment/payment.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { PaymentService } from './payment.service';
+import { JwtAuthGuard } from '../auth/jwt.auth.guard';
 
 @Controller('payment')
-export class PaymentController {}
+export class PaymentController {
+    constructor(private readonly paymentService: PaymentService) { }
+
+    @UseGuards(JwtAuthGuard)
+    @Post()
+    async createPayment(@Body() body: { bankAccountId: string, amount: number, date: Date, description: string }) {
+        return this.paymentService.createPayment(body);
+    }
+
+}

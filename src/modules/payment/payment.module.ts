96608df1476680e5f86530a44bd02c5787def6bd
Luiz Otávio <luizotavioautomacao@gmail.com>
2024-07-04 17:03:50 -0300

    feat: add bankModule in PaymentModule

--- src/modules/payment/payment.module.ts
+++ src/modules/payment/payment.module.ts
@@ -3,11 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
 import { PaymentModel, PaymentSchema } from './payment.schema';
+import { BankAccountModule } from '../bank-account/bank-account.module';
 
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: PaymentModel.name, schema: PaymentSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: PaymentModel.name, schema: PaymentSchema }]),
+    BankAccountModule
+  ],
   controllers: [PaymentController],
   providers: [PaymentService],
 })
-export class PaymentModule {}
+export class PaymentModule { }

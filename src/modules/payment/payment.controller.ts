1b6e1dfb0b8837602d3fccae3959b3c968bd9379
Luiz Otávio <luizotavioautomacao@gmail.com>
2024-07-04 17:04:54 -0300

    feat: add rules payment when creating a payment

--- src/modules/payment/payment.controller.ts
+++ src/modules/payment/payment.controller.ts
@@ -1,15 +1,27 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body, Request, UseGuards } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { JwtAuthGuard } from '../auth/jwt.auth.guard';
+import { BankAccountService } from '../bank-account/bank-account.service';
+import { BankAccount } from '../bank-account/bank-account.interface';
+import { Payment } from './payment.interface';
+import { InvalidBankAccount } from 'src/presentation/errors/invalid-bank-account';
+import { InsufficientBalance } from 'src/presentation/errors/insufficient-balance';
 
 @Controller('payment')
 export class PaymentController {
-    constructor(private readonly paymentService: PaymentService) { }
+    constructor(
+        private readonly paymentService: PaymentService,
+        private readonly bankAccountService: BankAccountService
+    ) { }
 
     @UseGuards(JwtAuthGuard)
     @Post()
-    async createPayment(@Body() body: { bankAccountId: string, amount: number, date: Date, description: string }) {
-        return this.paymentService.createPayment(body);
+    async createPayment(@Request() req, @Body() body: { bankAccountId: string, amount: number, date: Date, description: string }): Promise<Payment | Error> {
+        const { bankAccountId, amount, date, description } = body
+        const bankAccount: BankAccount = await this.bankAccountService.getBankAccountById(bankAccountId)
+        if (req.user._id != bankAccount.userId) return new InvalidBankAccount()
+        if (bankAccount.initialBalance < amount) return new InsufficientBalance()
+        return this.paymentService.createPayment({ payment: body, bankAccount });
     }
 
 }
--- src/modules/payment/payment.service.ts
+++ src/modules/payment/payment.service.ts
@@ -1,16 +1,31 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { PaymentDocument, PaymentModel } from './payment.schema';
-import { Payment } from './payment.interface';
+import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
+import { PaymentDocument, PaymentModel } from './payment.schema'
+import { Payment } from './payment.interface'
+import { BankAccount } from '../bank-account/bank-account.interface'
+import { BankAccountService } from '../bank-account/bank-account.service'
+
+interface PaymentParams {
+    bankAccount: BankAccount,
+    payment: Payment
+}
 
 @Injectable()
 export class PaymentService {
-    constructor(@InjectModel(PaymentModel.name) private paymentModel: Model<PaymentDocument>) { }
+    constructor(
+        @InjectModel(PaymentModel.name) private paymentModel: Model<PaymentDocument>,
+        private bankAccountService: BankAccountService,
+    ) { }
 
-    async createPayment(createPayment: Payment): Promise<PaymentModel> {
-        const createdPayment = new this.paymentModel(createPayment);
-        return createdPayment.save();
+    async createPayment(paymentParams: PaymentParams): Promise<PaymentModel> {
+        const { bankAccount, payment } = paymentParams
+        payment.date = new Date()
+        const createdPayment = new this.paymentModel(paymentParams.payment)
+        createdPayment.save()
+        bankAccount.initialBalance = bankAccount.initialBalance - payment.amount
+        this.bankAccountService.updateBankAccount(bankAccount)
+        return createdPayment
     }
 
 }

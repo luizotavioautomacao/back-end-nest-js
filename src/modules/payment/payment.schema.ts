ed06c2852ad92182effafc6b46abe1fd0a57729f
Luiz Otávio <luizotavioautomacao@gmail.com>
2024-07-04 00:22:12 -0300

    feat: add payment model

---
+++ src/modules/payment/payment.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type PaymentDocument = Payment & Document;
+
+@Schema()
+export class Payment {
+  @Prop({ required: true })
+  bankAccountId: string;
+
+  @Prop({ required: true })
+  amount: number;
+
+  @Prop({ required: true })
+  date: Date;
+
+  @Prop()
+  description: string;
+}
+
+export const PaymentSchema = SchemaFactory.createForClass(Payment);

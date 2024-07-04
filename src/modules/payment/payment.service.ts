41025749fff4e4000a08fd668edc2ccede73607f
Luiz Otávio <luizotavioautomacao@gmail.com>
2024-07-04 00:46:25 -0300

    feat: add create payment in service

--- src/modules/payment/payment.service.ts
+++ src/modules/payment/payment.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { PaymentDocument, PaymentModel } from './payment.schema';
+import { Payment } from './payment.interface';
 
 @Injectable()
-export class PaymentService {}
+export class PaymentService {
+    constructor(@InjectModel(PaymentModel.name) private paymentModel: Model<PaymentDocument>) { }
+
+    async createPayment(createPayment: Payment): Promise<PaymentModel> {
+        const createdPayment = new this.paymentModel(createPayment);
+        return createdPayment.save();
+    }
+
+}

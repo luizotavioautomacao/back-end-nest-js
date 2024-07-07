a1baf626b07bae2cebc25fef8f70a403646df502
Luiz Otávio <luizotavioautomacao@gmail.com>
2024-07-07 20:13:16 -0300

    feat: ensure AddBankController returns a badRequest if validation fails

--- src/presentation/controllers/bank-account/add-bank-account-controller.ts
+++ src/presentation/controllers/bank-account/add-bank-account-controller.ts
@@ -1,13 +1,15 @@
-import { IController } from "src/presentation/protocols/controller";
-import { IHttpRequest, IHttpResponse } from "src/presentation/protocols/http";
-import { IValidation } from "src/presentation/protocols/validation";
+import { badRequest } from "../../helpers/http-helper"
+import { IController } from "src/presentation/protocols/controller"
+import { IHttpRequest, IHttpResponse } from "src/presentation/protocols/http"
+import { IValidation } from "src/presentation/protocols/validation"
 
 export class AddBankController implements IController {
     constructor(
         private readonly validation: IValidation
     ) { }
     async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
-        this.validation.validate(httpRequest.body)
+        const error = this.validation.validate(httpRequest.body)
+        if (error) return badRequest(error)
         return new Promise(resolve => resolve(null))
     }
 }

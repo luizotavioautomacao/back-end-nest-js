1da2bb149a6af2417fa9cc83745b61031a02931c
Luiz Otávio <luizotavioautomacao@gmail.com>
2024-07-08 01:36:05 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/db-add-bank-account.spec.ts
+++ src/data/usecases/db-add-bank-account.spec.ts
@@ -3,18 +3,35 @@ import { IAddBankAccountRepository } from "../protocols/db/bank-account/add-bank
 import { DbAddBankAccount } from "./db-add-bank-account"
 import { AddBankAccountModel } from "src/domain/usecases/add-bank-account"
 
+interface SutTypes {
+  sut: DbAddBankAccount,
+  addBankAccountRepositoryStub: IAddBankAccountRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addBankAccountRepositoryStub = makeAddBankAccountRepository()
+  const sut = new DbAddBankAccount(addBankAccountRepositoryStub)
+  return {
+    sut,
+    addBankAccountRepositoryStub
+  }
+}
+
+const makeAddBankAccountRepository = (): IAddBankAccountRepository => {
+  class AddBankAccountRepositoryStub implements IAddBankAccountRepository {
+    async add(bankAccountData: AddBankAccountModel): Promise<void> {
+      return new Promise(resolve => resolve())
+    }
+  }
+  return new AddBankAccountRepositoryStub()
+}
+
+
 describe('DbAddBankAccount Usecase', () => {
 
   test('Should call AddBankAccountRepository with correct values', () => {
-    class AddBankAccountRepositoryStub implements IAddBankAccountRepository {
-      async add(bankAccountData: AddBankAccountModel): Promise<void> {
-        return new Promise(resolve => resolve())
-      }
-    }
-
-    const addBankAccountRepositoryStub = new AddBankAccountRepositoryStub()
+    const { sut, addBankAccountRepositoryStub } = makeSut()
     const addSpy = jest.spyOn(addBankAccountRepositoryStub, 'add')
-    const sut = new DbAddBankAccount(addBankAccountRepositoryStub)
     sut.add(mockAddBankAccount)
     expect(addSpy).toHaveBeenCalledWith(mockAddBankAccount)
   })

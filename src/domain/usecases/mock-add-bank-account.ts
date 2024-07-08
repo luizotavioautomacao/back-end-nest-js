bfe8aca1fcd2399356f240bf98f4fdf1685a2d26
Luiz Otávio <luizotavioautomacao@gmail.com>
2024-07-08 02:43:37 -0300

    fix: mock add bank account

--- src/domain/usecases/mock-add-bank-account.ts
+++ src/domain/usecases/mock-add-bank-account.ts
@@ -2,7 +2,15 @@ import { AddBankAccountModel } from "./add-bank-account";
 
 export const mockAddBankAccount: AddBankAccountModel =
 {
-    name: "Luiz Otávio",
+    name: "Inter",
     type: "corrente",
     initialBalance: 0,
 }
+
+export const mockAddBankAccount2: AddBankAccountModel =
+{
+    name: "Nubank",
+    type: "corrente",
+    initialBalance: 5000,
+}
+
